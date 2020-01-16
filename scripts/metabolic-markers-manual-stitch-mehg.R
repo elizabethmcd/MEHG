e5319486e67e86e8349afcf4e96d4c60ab21c1ab
Elizabeth McDaniel <emcdaniel@wisc.edu>
2020-01-16 12:09:04 -0600

    preprint final changes

--- scripts/metabolic-markers-manual-stitch-mehg.R
+++ scripts/metabolic-markers-manual-stitch-mehg.R
@@ -4,10 +4,10 @@ library(grid)
 library(gridExtra)
 
 # markers and metadata files
-markers = read.csv("results/metabolic_results/raw-metabolic-marker-results.csv")
-metadata = read.csv("files/stats/quals/mehg-markers-metadata.csv", header=FALSE, stringsAsFactors = FALSE)
-nitrogen = read.csv("results/metabolic_results/all-nitrogen-markers.csv")
-mercury = read.csv("results/metabolic_results/mercury-markers.csv")
+markers = read.csv("files_and_results/metabolic_summs_results/raw-metabolic-marker-results.csv")
+metadata = read.csv("files_and_results/stats/quals/mehg-markers-metadata.csv", header=FALSE, stringsAsFactors = FALSE)
+nitrogen = read.csv("files_and_results/metabolic_summs_results/all-nitrogen-markers.csv")
+mercury = read.csv("files_and_results/metabolic_summs_results/mercury-markers.csv")
 # column names
 colnames(markers)[1] = c("genome")
 colnames(nitrogen)[1] = c("genome")
@@ -39,28 +39,31 @@ agg <- aggregate(test[1:exclude], list(test$group), mean)
 carbon = cbind(agg$Group.1, agg[,c("codhC_TIGR00316", "codhD_TIGR00381", "codh_catalytic_TIGR01702", "folD", "metF")])
 colnames(carbon) = c("group", "codhC", "codhD", "codh catalytic", "folD", "metF")
 carbon.melted = melt(carbon, id.vars= "group") %>%  mutate(group=factor(group), group = factor(group, levels = rev(levels(group))))
-carbon.plot = carbon.melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() +  scale_fill_gradient(low="gray85", high="brown4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
+carbon.plot = carbon.melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() +  scale_fill_gradient(low="white", high="brown4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
 carbon.formatted = carbon.plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank()) + labs(x=NULL, y=NULL) +  scale_y_discrete(expand=c(0,0))
 carbon.clean = carbon.formatted + theme(legend.position="none")
+carbon.clean
   # sulfur
 sulfur = cbind(agg$Group.1, agg[,c("dsrA_TIGR02064", "dsrB_TIGR02066", "dsrD_PF08679", "sat_TIGR00339")])
 colnames(sulfur) = c("group", "dsrA", "dsrB", "dsrD", "sat")
 sulfur.melted = melt(sulfur, id.vars="group") %>% mutate(group=factor(group), group = factor(group, levels = rev(levels(group))))
-sulfur.plot = sulfur.melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() +  scale_fill_gradient(low="grey85", high="orchid4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
+sulfur.plot = sulfur.melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() +  scale_fill_gradient(low="white", high="orchid4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
 sulfur.formatted = sulfur.plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank()) + labs(x="Sulfur", y=NULL) +  scale_y_discrete(expand=c(0,0))
 sulfur.clean = sulfur.formatted + theme(legend.position="none")
+sulfur.clean
   # nitrogen
 nitrogen = cbind(agg$Group.1, agg[,c("nifDHK", "narGH", "nirBDK", "norBC", "nosDZ")])
 colnames(nitrogen) = c("group", "nifDHK", "narGH", "nirBDK", "norBC", "nosDZ")
 nitrogen.melted <- melt(nitrogen, id.vars="group") %>% mutate(group=factor(group), group = factor(group, levels = rev(levels(group))))
-nitrogen.plot <- nitrogen.melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() +  scale_fill_gradient(low="grey85", high="steelblue4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
+nitrogen.plot <- nitrogen.melted %>% ggplot(aes(x=variable, y=(group), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() +  scale_fill_gradient(low="white", high="steelblue4") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
 nitrogen.formatted = nitrogen.plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.y=element_blank()) + labs(x="Nitrogen", y=NULL) +  scale_y_discrete(expand=c(0,0))
 nitrogen.clean = nitrogen.formatted + theme(legend.position="none")
+nitrogen.clean
   # metal resistance
 metals = cbind(agg$Group.1, agg[,c("hgcA", "arsC_thioredoxin", "sulfur_dioxygenase_sdo")])
 colnames(metals) = c("group", "hgcA", "arsC", "sdo")  
 metals.melted = melt(metals, id.vars = "group") %>% mutate(group=factor(group), group = factor(group, levels = rev(levels(group))))
-metals.plot = metals.melted %>% ggplot(aes(x=variable, y=(rev(group)), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() +  scale_fill_gradient(low="grey85", high="midnightblue") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
+metals.plot = metals.melted %>% ggplot(aes(x=variable, y=(rev(group)), fill=value)) + geom_tile(color='black',size=0.5,aes(width=1, height=1)) + coord_fixed() +  scale_fill_gradient(low="white", high="midnightblue") + theme(panel.grid = element_blank(), panel.border = element_blank(), plot.margin = unit(c(0, 0, 0, 0), "cm"))
 metals.formatted = metals.plot + scale_x_discrete(position="top", expand=c(0,0)) + theme(axis.text.x.top=element_text(angle=85, hjust=0, face="italic"), axis.ticks.x=element_blank(), axis.ticks.y=element_blank()) + labs(x=NULL, y=NULL) +  scale_y_discrete(expand=c(0,0))
 metals.clean = metals.formatted + theme(legend.position="none")
 metals.formatted
