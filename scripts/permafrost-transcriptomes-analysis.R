90e11648d3160678ba306ca9f273a63f734560e8
Elizabeth McDaniel <emcdaniel@wisc.edu>
2020-05-22 10:12:43 -0500

    updates to permafrost transcription analysis, locus tag ids for hgcA neighborhood

--- scripts/permafrost-transcriptomes-analysis.R
+++ scripts/permafrost-transcriptomes-analysis.R
@@ -2,15 +2,15 @@ library(tximport)
 library(readr)
 library(tibble)
 library(dplyr)
-library(reshape2)
+library(reshape)
 library(gtable)
 library(grid)
 library(gridExtra)
 library(viridis)
-library(ggplot2)
+library(tidyverse)
 
 # kallisto directory for mapped transcriptomes to methylators
-meth_dir <- "/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/kallisto_mapping"
+meth_dir <- "kallisto_mapping/"
 meth_samples <- read.table(file.path(meth_dir, "permafrost-metadata.txt"), header=TRUE)
 meth_files <- file.path(meth_dir, meth_samples$sample, "abundance.h5")
 rownames(meth_samples) <- meth_samples$sample
@@ -21,45 +21,29 @@ meth.kallisto <- tximport(meth_files, type="kallisto", txOut = TRUE)
 # counts file
 meth.counts <- as.data.frame(meth.kallisto)
 final.meth.counts <- rownames_to_column(meth.counts, var="genome_name")
-meth.counttable <- final.meth.counts[, c(1, 28:53)]
+meth.counttable <- final.meth.counts[, c(1:27)]
 
 # ids to pick certain genomes
 ids <- read.delim("~/Desktop/McMahon-Lab/MeHg-Projects/MEHG/kallisto_mapping/perma-meth-ids.txt", sep="\t", header=TRUE)
 counttable.ids <- left_join(meth.counttable, ids)
 
-# TPM normalization
-## Merge with annotations to divide by gene length for counts in all samples
-# also check if this is in the cloud
-meth_prokka = read.delim("/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files/permafrostTranscription/permafrost-methylator-prokka-annotations.txt", sep="\t", header=FALSE)
+## Merge with annotations to get gene lengths
+meth_prokka = read.delim("/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files_and_results/permafrostTranscription/permafrost-methylator-prokka-annotations.txt", sep="\t", header=FALSE)
 colnames(meth_prokka) <- c("genome_name", "prokka_annotation", "size_bp", "accession")
-meth_counts_annots <- left_join(meth_prokka, counttable.ids)
-meth_counts_annots$size_kbp = meth_counts_annots$size_bp / 1000
-
-# Divide by gene lengths
-meth_counts_rpk = as.data.frame(lapply(meth_counts_annots[,c(-1,-2,-3, -4, -31, -32)], function(x) {
-  (x / meth_counts_annots$size_kbp)
-}))
-# per million factor
-meth_counts_rpk["PM", ] = (colSums(meth_counts_rpk[1:26]) / 1000000)
-# divide by per million factor
-meth_counts_tpm = as.data.frame(lapply(meth_counts_rpk, function(x) x/tail(x,1) ))
-# get metadata back to aggregate by genome_id
-meth_counts_tpm = meth_counts_tpm[-387690, ]
-meth_counts_tpm$locus_tag = meth_counts_annots$genome_name
-meth_counts_tpm$genome_name = meth_counts_annots$genome
-meth_counts_totals = aggregate(meth_counts_tpm[1:26], list(meth_counts_tpm$genome_name), sum)
 
 # permafrost results from archived set of metadata
 # merge with metadata to get genome sizes to get relative expression by phyla
-meth_metadata = read.csv("/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files/archived/archived-metadata/archived-methylator-metadata.csv")
+meth_metadata = read.csv("/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files_and_results/archived/archived-metadata/archived-methylator-metadata.csv")
 peat_phyla = meth_metadata %>% select("genome_name", "Phylum")
-colnames(meth_counts_totals)[1] = "genome_name"
-meth_counts_totals_table = left_join(meth_counts_totals, peat_phyla)
+colnames(peat_phyla) <- c("genome", "Phylum")
+meth_counts_totals_table = left_join(counttable.ids, peat_phyla)
 meth_expression_average = aggregate(meth_counts_totals_table[2:27], list(meth_counts_totals_table$Phylum), sum)
+genome_expression_average = aggregate(meth_counts_totals_table[2:27], list(meth_counts_totals_table$genome), sum)
 meth_expression_average$avg_expression = rowSums(meth_expression_average[2:27]) / 26
+genome_expression_average$avg_expression = rowSums(genome_expression_average[2:27]) / 26
 
 # hgcA locus tags 
-hgcA = read.delim("/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files/archived/tree-tax-files/hgcA-locus-tags-phyla.txt", sep="\t", header=FALSE)
+hgcA = read.delim("/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files_and_results/archived/tree-tax-files/hgcA-locus-tags-phyla.txt", sep="\t", header=FALSE)
 colnames(hgcA) = c("genome_name", "locus_tag", "Phylum")
 hgcA = hgcA %>% select(genome_name, locus_tag)
 mehg_peat = meth_metadata %>% filter(Study=="Woodcroft2018") 
@@ -67,31 +51,31 @@ peat_locus_tags = left_join(mehg_peat, hgcA)
 peat_hgcA = peat_locus_tags %>% select(c(Phylum, locus_tag))
 
 # counts of hgcA
-meth_counts_norm = meth_counts_tpm
-hgcA_counts = left_join(peat_hgcA, meth_counts_norm)
-write.csv(hgcA_counts, "/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files/permafrostTranscription/meth-counts-hgcA-norm.csv", quote=FALSE, row.names=FALSE)
+colnames(counttable.ids)[1] = c("locus_tag")
+hgcA_counts = left_join(peat_hgcA, counttable.ids)
+write.csv(hgcA_counts, "/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files_and_results/permafrostTranscription/meth-counts-hgcA-norm.csv", quote=FALSE, row.names=FALSE)
 hgcA_non_agg_zeros = hgcA_counts %>% select(c(Phylum, locus_tag, 4,6,9,10,12,14,16,17,18,20:26,28))
 hgcA_phyla_total = aggregate(hgcA_counts[3:28], list(hgcA_counts$Phylum),sum)
 hgcA_no_zeros = hgcA_phyla_total %>% select(c(Group.1, 3,5,8,9,11,13,15,16,17,19:25,27))
 hgcA_counts.m = melt(hgcA_phyla_total, id.vars="Group.1")
 hgcA_no_zeros.m = melt(hgcA_no_zeros, id.vars="Group.1")
-sample_list = c("counts.20120600_S2M",
-                "counts.20120800_S1M",
-                "counts.20120600_S2D",
-                "counts.20120700_S1D",
-                "counts.20120800_S1X",
-                "counts.20100900_E2S",
-                "counts.20110600_E1M",
-                "counts.20120600_E1M",
-                "counts.20120700_E3M",
-                "counts.20120800_E2M",
-                "counts.20100900_E1D",
-                "counts.20110600_E1D",
-                "counts.20110700_E1D",
-                "counts.20120600_E1D",
-                "counts.20120700_E3D",
-                "counts.20120800_E2D",
-                "counts.20120800_E3D")
+sample_list = c("abundance.20120600_S2M",
+                "abundance.20120800_S1M",
+                "abundance.20120600_S2D",
+                "abundance.20120700_S1D",
+                "abundance.20120800_S1X",
+                "abundance.20100900_E2S",
+                "abundance.20110600_E1M",
+                "abundance.20120600_E1M",
+                "abundance.20120700_E3M",
+                "abundance.20120800_E2M",
+                "abundance.20100900_E1D",
+                "abundance.20110600_E1D",
+                "abundance.20110700_E1D",
+                "abundance.20120600_E1D",
+                "abundance.20120700_E3D",
+                "abundance.20120800_E2D",
+                "abundance.20120800_E3D")
 hgcA_no_zeros.m$variable = factor(hgcA_no_zeros.m$variable, levels = c(sample_list))
 
 # total hgcA by sample
@@ -101,18 +85,25 @@ colnames(sample_totals) = c("total", "sample")
 sample_totals$sample = factor(sample_totals$sample, levels=c(sample_list))
 
 # heatmap of hgcA total expression
-hgcA_plot = ggplot(hgcA_counts.m, aes(x=fct_rev(Group.1), y=variable, fill=value)) + geom_tile(color="white") + scale_fill_viridis(option="magma",alpha=1, begin=0, end=0.95, direction=-1) + theme_bw()
+hgcA_plot = ggplot(hgcA_counts.m, aes(fct_rev(Group.1), y=variable, fill=value)) + geom_tile(color="white") + scale_fill_viridis(option="viridis",alpha=1, begin=0, end=0.95, direction=-1) + theme_bw()
 hgcA_plot2 = hgcA_plot + theme(axis.text.x= element_text(angle=85, hjust=1)) + guides(fill = guide_colorbar(nbin = 10))
 hgcA_plot2
 
 # with no zeros of total expression by phyla
-no_zeros_plot = ggplot(hgcA_no_zeros.m, aes(x=Group.1, y=fct_rev(variable), fill=value)) + geom_tile(color="white") + scale_fill_viridis(option="viridis",alpha=1, begin=0, end=1, direction=-1) + theme_bw()
+no_zeros_plot = ggplot(hgcA_no_zeros.m, aes(x=(Group.1), y=fct_rev(variable), fill=value)) + geom_tile(color="white") + scale_fill_viridis(option="viridis",alpha=1, begin=0, end=1, direction=-1, limits=c(0,165), breaks=c(0, 25, 50, 75, 100, 165), rescaler = function(x, to = c(0, 0.8), from = NULL) {
+  ifelse(x<100, 
+         scales::rescale(x,
+                         to = to,
+                         from = c(min(x, na.rm = TRUE), 100)),
+         1)
+  }) + theme_bw()
+no_zeros_plot
 no_zeros_plot2 = no_zeros_plot + theme(axis.text.x= element_text(angle=85, hjust=1)) + guides(fill = guide_colorbar(nbin = 10)) 
 no_zeros_plot3 = no_zeros_plot2 + theme_gray() +
   theme(legend.position = "bottom", legend.direction = "horizontal",
         legend.title = element_text(size = 15), legend.key.size = unit(1,"cm"),
         legend.text = element_text(size = 7))
-no_zeros_plot2
+no_zeros_plot3
 
 # total hgcA by sample
 sample_totals = as.data.frame(colSums(hgcA_no_zeros[,c(2:18)]))
@@ -122,7 +113,7 @@ sample_totals$sample = factor(sample_totals$sample, levels=c(sample_list))
 
 # plot bar graph of total hgcA per sample
 colors = c("bog", "bog", "fen", "fen", "bog", "bog", "bog", "fen", "fen", "fen","fen","fen","fen","fen", "fen", "fen", "fen")
-hgcA_sample = sample_totals %>% ggplot(aes(x=fct_rev(sample), y=total, fill=colors)) + geom_bar(stat="identity", width=.70) + coord_flip() + scale_y_continuous(limits=c(0,350), expand= c(0,0)) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank()) + scale_fill_manual("legend", values=c("bog"="darkgreen","fen"="midnightblue"))
+hgcA_sample = sample_totals %>% ggplot(aes(x=fct_rev(sample), y=total, fill=colors)) + geom_bar(stat="identity", width=.70) + coord_flip() + scale_y_continuous(limits=c(0,200), expand= c(0,0)) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank()) + scale_fill_manual("legend", values=c("bog"="darkgreen","fen"="midnightblue"))
 hgcA_sample
 
 # average expression of each methylator
@@ -175,13 +166,13 @@ hgcA_sample_clean
 # together
 grid = grid.arrange(avg_clean, legend, hm.clean, hgcA_sample_clean, nrow=2, ncol=2, widths=c(30,40), heights=c(40,60))
 # save grid 
-ggsave(file="~/Desktop/mehg-grid-axes.png", grid, height=10, width=15, units=c("cm"))
+ggsave(file="figs/2020-05-22-permafrost-transcription-grid.png", grid, height=10, width=15, units=c("cm"))
 
 
 ### comparison to housekeeping gene expression
-rpoB = read.delim("/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files/permafrostTranscription/rpoB-locus-tags.tsv", header=TRUE, sep="\t")
+rpoB = read.delim("/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files_and_results/permafrostTranscription/rpoB-locus-tags.tsv", header=TRUE, sep="\t")
 peat_rpoB = rpoB %>% select(c(Phyla, locus_tag))
-rpoB_counts = left_join(peat_rpoB, meth_counts_norm)
+rpoB_counts = left_join(peat_rpoB, counttable.ids)
 rpoB_no_zeros = rpoB_counts %>% select(c(Phyla, locus_tag, 4,6,9,10,12,14,16,17,18,20:26,28))
 rpoB_agg = aggregate(rpoB_no_zeros[3:19], list(rpoB_no_zeros$Phyla), sum)
 rpoB.m = melt(rpoB_agg, id.vars="Group.1")
@@ -195,7 +186,12 @@ write.csv(rpo.sub, "~/Desktop/rpo-counts.csv", quote=FALSE, row.names=FALSE)
 colnames(hgcA.test) = c("variable", "hgcA.counts")
 merged.counts = left_join(rpo.sub, hgcA.test)
 
-# separate tottals
+hgcA.rpo$marker = c("hgcA")
+colnames(hgcA.rpo)[1] = c("phyla")
+rpo.sub$marker = c("rpo")
+colnames(rpo.sub)[1] = c("phyla")
+
+# separate totals
   #rpoB
 rpoB.plt = ggplot(rpo.sub, aes(x=Group.1, y=value)) + geom_boxplot()
 rpoB.plt + geom_jitter(shape=15, position=position_jitter(0.2)) + theme_classic() + theme(axis.text.x=element_text(angle=85, hjust=1))
@@ -204,9 +200,19 @@ hgcA.rpo.plt = ggplot(hgcA.rpo, aes(x=Group.1, y=value)) + geom_boxplot()
 hgcA.rpo.plt + geom_jitter(shape=15, position=position_jitter(0.2)) + theme_classic() + theme(axis.text.x=element_text(angle=85, hjust=1))
 
 # together with variable as marker instead of sampling depth
-hgcA.var = read.csv("~/Desktop/hgcA-counts-var.csv")
-rpoB.var = read.csv("~/Desktop/rpo-counts-var.csv")
-combined = rbind(hgcA.var, rpoB.var)
-comb.plt = ggplot(combined, aes(x=phyla, y=value, fill=variable)) + geom_boxplot()
+combined = rbind(hgcA.rpo, rpo.sub)
+comb.plt = ggplot(combined, aes(x=phyla, y=value, fill=marker)) + geom_boxplot() + geom_jitter()
+comb.plt
 comb.form = comb.plt + theme_classic() + theme(axis.text.x=element_text(angle=85, hjust=1))
+comb.form
 ggsave(comb.form, filename="/Users/emcdaniel/Desktop/McMahon-Lab/MeHg-Projects/MEHG/files/permafrostTranscription/hgcA-vs-rpoB-expression.png", width=30, height=20, units=c("cm"))
+
+# divide hgcA by rpoB
+hgcA.counts <- hgcA.rpo %>% select(phyla, variable, value)
+rpo.counts <- rpo.sub %>% select(phyla, variable, value)
+colnames(hgcA.counts)[3] <- c("hgcA.val")
+colnames(rpo.counts)[3] <- c("rpo.val")
+hgcA_rpo_combined <- cbind(hgcA.counts, rpo.counts$rpo.val)
+colnames(hgcA_rpo_combined) <- c("phyla", "sample", "hgcA.val", "rpo.val")
+hgcA_rpo_combined$hgcA_over_rpo <- hgcA_rpo_combined$hgcA.val / hgcA_rpo_combined$rpo.val
+hgcA_rpo_combined[is.na(hgcA_rpo_combined)] <- 0
